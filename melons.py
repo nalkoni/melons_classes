946ad7d20025137593d86cf777200e61c4972c99
Hackbright Student <info@hackbrightacademy.com>
2016-07-15 12:52:56 -0700

    Melons_Order Part 2- Post Code Review

--- melons.py
+++ melons.py
@@ -14,7 +14,15 @@ class AbstractMelonOrder(object):
         """Calculate price."""
 
         base_price = 5
+       
+        if self.species == "christmas":
+            base_price *= 1.5
+
         total = (1 + self.tax) * self.qty * base_price
+
+        if self.order_type == "international" and self.qty < 10:
+            total += 3 
+
         return total
 
     def mark_shipped(self):
@@ -51,56 +59,3 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Return the country code."""
 
         return self.country_code
-        """This file should have our order classes in it."""
-        class AbstractMelonOrder(object):
-            """DOCSTRING"""
-
-            def __init__(self, species, qty, order_type, shipped=False):
-                """Initialize melon order attributes"""
-
-                self.species = species
-                self.qty = qty
-                self.order_type = order_type
-
-
-            def get_total(self):
-                """Calculate price."""
-
-                base_price = 5
-                total = (1 + self.tax) * self.qty * base_price
-                return total
-
-            def mark_shipped(self):
-                """Set shipped to true."""
-
-                self.shipped = True
-
-
-        class DomesticMelonOrder(AbstractMelonOrder):
-            """A domestic (in the US) melon order."""
-
-            tax = 0.08
-
-            def __init__(self, species, qty):
-                """Initialize melon order attributes"""
-
-                super(DomesticMelonOrder, self).__init__(species, qty, "domestic")
-
-
-        class InternationalMelonOrder(AbstractMelonOrder):
-            """An international (non-US) melon order."""
-
-            tax = 0.17
-
-            def __init__(self, species, qty, country_code):
-                """Initialize melon order attributes"""
-
-                super(InternationalMelonOrder, self).__init__(species, qty, "international")
-                
-                self.country_code = country_code
-
-
-            def get_country_code(self):
-                """Return the country code."""
-
-                return self.country_code
